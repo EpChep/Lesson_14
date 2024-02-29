3ba57c0559e8851981bf17d981a0ae57042a1e7d
Eprem Chepnyan <156126721+EpChep@users.noreply.github.com>
2024-02-29 22:13:14 +0300

    Commit

---
+++ src/test/java/MtsTest.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
+
+public class MtsTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.mts.by");
+        try {
+            WebElement cookieAgreeButton = driver.findElement(By.cssSelector("#cookie-agree"));
+            cookieAgreeButton.click();
+        } catch (NoSuchElementException e) {
+        }
+    }
+    @AfterEach
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void testCommunicationServices(){
+        WebElement phoneNumberField = driver.findElement(By.cssSelector("#connection-phone"));
+        assert phoneNumberField.getAttribute("placeholder").equals("Номер телефона");
+
+        WebElement sumField = driver.findElement(By.cssSelector("#connection-sum"));
+        assert sumField.getAttribute("placeholder").equals("Сумма");
+
+        WebElement emailField = driver.findElement(By.cssSelector("#connection-email"));
+        assert emailField.getAttribute("placeholder").equals("E-mail для отправки чека");
+
+        WebElement paymentButton = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button"));
+        paymentButton.click();
+
+        WebElement homeInternetButton = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[1]/div[1]/div[2]/ul/li[2]/p"));
+        homeInternetButton.click();
+
+        WebElement phoneNumberField2 = driver.findElement(By.cssSelector("#internet-phone"));
+        assert  phoneNumberField2.getAttribute("placeholder").equals("Номер абонента");
+
+        WebElement sumField2 = driver.findElement(By.cssSelector("#internet-sum"));
+        assert  sumField2.getAttribute("placeholder").equals("Сумма");
+
+        WebElement emailField2 = driver.findElement(By.cssSelector("#internet-email"));
+        assert emailField2.getAttribute("placeholder").equals("E-mail для отправки чека");
+
+        paymentButton.click();
+
+        WebElement installmentPlanButton = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[1]/div[1]/div[2]/ul/li[3]/p"));
+        installmentPlanButton.click();
+
+        WebElement accountNumberField = driver.findElement(By.cssSelector("#score-instalment"));
+        assert  accountNumberField.getAttribute("placeholder").equals("Номер счета на 44");
+
+        WebElement sumField3 = driver.findElement(By.cssSelector("#instalment-sum"));
+        assert  sumField3.getAttribute("placeholder").equals("Сумма");
+
+        WebElement emailField3 = driver.findElement(By.cssSelector("#instalment-email"));
+        assert  emailField3.getAttribute("placeholder").equals("E-mail для отправки чека");
+
+        paymentButton.click();
+
+        WebElement debtButton = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[1]/div[1]/div[2]/ul/li[4]/p"));
+        debtButton.click();
+
+        WebElement accountNumber2 = driver.findElement(By.cssSelector("#score-arrears"));
+        assert  accountNumber2.getAttribute("placeholder").equals("Номер счета на 2073");
+
+        WebElement sumField4 = driver.findElement(By.cssSelector("#arrears-sum"));
+        assert  sumField4.getAttribute("placeholder").equals("Сумма");
+
+        WebElement emailField4 = driver.findElement(By.cssSelector("#arrears-email"));
+        assert  emailField3.getAttribute("placeholder").equals("E-mail для отправки чека");
+
+    }
+}
